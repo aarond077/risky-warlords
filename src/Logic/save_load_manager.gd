41d9d618e4aaeb4453505bf8107fbda0027cbeda
aaron <aaron.diether@gmail.com>
2024-04-29 17:22:04 +0200

    added scene switch logic, scene save/load logic and renamed skirmish into scenario

---
+++ src/Logic/save_load_manager.gd
@@ -0,0 +1,37 @@
+extends Node
+
+#class_name SaveLoadModule
+ 
+var file : FileAccess     # File object opened by the FileAccess library
+var file_path : String     # File path to the requested file
+var password : String     # Password for file encryption and decryption
+ 
+# Fill the file data in the local variables
+func initialize(init_path : String, init_password : String) -> void:
+	file_path = init_path
+	password = init_password
+ 
+# Clear the file data in the local variables
+func clear() -> void:
+	file = null
+	file_path = ""
+	password = ""
+
+# Open a file encrypted with the given password
+func open_file(access : FileAccess.ModeFlags) -> int:
+	# Try opening an encrypted file with write access
+	file = FileAccess.open_encrypted_with_pass(file_path, access, password)	 
+	
+	print(FileAccess.file_exists(file_path))
+	# Return the assigned file index (handle)
+	return FileAccess.get_open_error() if (file == null) else OK	 
+# Open a file encrypted with the given password
+func close_file() -> void:
+	file = null
+	
+# Serialize the object's properties and write them to the file with the given ID
+func serialize(object) -> void:
+	object.serialize(file) 
+# Read the object's properties from the given file and deserialize them
+func deserialize(object) -> void:
+	object.deserialize(file)
---
+++ src/Logic/scene_manager.gd
@@ -0,0 +1,48 @@
+extends Node
+
+# A collection of scenes in the game. Scenes are added through the Inspector panel
+@export var scenes : Dictionary = {}
+# Alias of the currently selected scene
+var current_scene_name : String = ""
+	
+# Description: Find the initial scene as defined in the project settings
+func _ready() -> void:
+	var main_scene : StringName = ProjectSettings.get_setting("application/run/main_scene")
+	current_scene_name = scenes.find_key(main_scene)
+	print(current_scene_name)
+ 
+
+
+# Description: Add a new scene to the scene collection
+# Parameter sceneAlias: The alias used for finding the scene in the collection
+# Parameter scenePath: The full path to the scene file in the file system
+func add_scene(scene_name : String, scene_path : String) -> void:
+	scenes[scene_name] = scene_path
+	
+# Description: Remove an existing scene from the scene collection
+# Parameter sceneAlias: The scene alias of the scene to remove from the collection
+func remove_scene(scene_name : String) -> void:
+	scenes.erase(scene_name)
+	
+# Description: Switch to the requested scene based on its alias
+# Parameter sceneAlias: The scene alias of the scene to switch to
+func switch_scene(scene_name : String) -> void:
+
+	current_scene_name = scene_name
+	get_tree().change_scene_to_file(scenes[scene_name])
+ 
+# Description: Restart the current scene
+func restart_scene() -> void:
+	get_tree().reload_current_scene()
+	
+# Description: Quit the game
+func quit_game() -> void:
+	get_tree().quit()
+	
+# Description: Return the number of scenes in the collection
+func get_scenes_size() -> int:
+	return scenes.size()
+	
+# Description: Returns the alias of the current scene
+func get_current_scene_name() -> String:
+	return current_scene_name
