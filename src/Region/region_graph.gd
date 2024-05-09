32296d9977ede819a7f562cf4bce6b0bb654d1a2
aaron <aaron.diether@gmail.com>
2024-05-10 00:57:00 +0200

    added interface for region graph and region node

---
+++ src/Region/region_graph.gd
@@ -0,0 +1,26 @@
+extends Node
+
+@onready var root : RegionNode
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func get_region_node(region_name : String):
+	pass
+	
+func add_region_node(new_node : RegionNode, neighbour_node : String):
+	pass
+	
+func remove_region_node(region_name : String):
+	pass
+
+func is_empty():
+	pass
+
+func size():
+	pass
--- src/Region/region_node.gd
+++ src/Region/region_node.gd
@@ -1,12 +1,14 @@
 extends Node
 
-class_name Region
+class_name RegionNode
 
 @onready var region_name : String
-@onready var region_ressource : String
+@onready var resource : String
+@onready var area : String
 @onready var building_slots : Array[String]
-@onready var region_army : Dictionary
 @onready var buildings : Array[String]
+@onready var region_army : Dictionary
+@onready var neighbours : Array[RegionNode]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
