f76fc1b6a8cb87b23d8496b988cd0805763327fd
gravitybeat <pouyan.w@gmail.com>
2024-06-14 11:28:12 +0200

    create_region_graph_from_file
    in region graph, added create_region_graph_from_file
    
    und aleksud indexes reversed

--- src/Region/region_graph.gd
+++ src/Region/region_graph.gd
@@ -6,6 +6,7 @@ var root : RegionNode
 var region_array : Array[RegionNode] #will get deleted after region graph is finished
 
 
+
 func load_resources_to_array():
 	var map_resources = DataFileManager.import_file("res://data/Maps/" + 
 		ScenarioDataManager.scenario_map_name + "/" + 
@@ -15,6 +16,35 @@ func load_resources_to_array():
 			region.resource = map_resources[region.region_name][0]
 			region.resource_factor = int(map_resources[region.region_name][1])
 
+#diese methode erzeugt den vollständigen RegionGraph
+func create_region_graph_from_file(map_name: String):
+	var region_graph = RegionGraph.new()
+	
+	load_regions_to_array()
+	var region_graph_dict_with_names = create_dict_with_map_names(map_name)
+	for region in region_graph_dict_with_names:
+		add_neighbours_to_region(region, region_graph_dict_with_names)
+	region_graph.root = region_array[0]
+	
+#diese methode füllt das Attribut neighbours aller RegionNodes
+func add_neighbours_to_region(region_name: String, region_graph_dict: Dictionary):
+	var neigbour_array = region_graph_dict[region_name]
+	var real_neighbour_array : Array[RegionNode]
+	
+	var region = return_region_node_from_name(region_name)
+	
+	for neighbour in neigbour_array:
+		real_neighbour_array.append(return_region_node_from_name(neighbour))
+	region.neighbours = real_neighbour_array
+	
+#diese methode findet für den namen eines RegionNodes 
+#den zugehörigen RegionNode und gibt ihn zurück
+func return_region_node_from_name(region_name: String):
+	for region in region_array:
+		if region.region_name == region_name:
+			return region
+
+
 func load_regions_to_array():
 	var map_regions = DataFileManager.import_file("res://data/Maps/" + 
 		ScenarioDataManager.scenario_map_name + "/" + 
@@ -23,6 +53,52 @@ func load_regions_to_array():
 		var new_region : RegionNode = RegionNode.new()
 		new_region.region_name = region
 		self.region_array.append(new_region)
+
+		# Beispiel: Absoluter Pfad eines Nodes
+		var node = new_region
+		var relative_path = node.get_path_to(node.get_parent())
+		print(relative_path)
+
+
+#diese methode erstellt eine adjazenzliste in einem dict
+#mit den namen der regionen
+func create_dict_with_map_names(map_name: String) -> Dictionary:
+		var areas = {}
+		# Öffne die Datei
+		if OK == OK:
+			var index_dict = DataFileManager.import_file(
+			"res://data/Maps/" + map_name + "/" + map_name + "Indexes.txt")
+		
+			var region_graph_dict = DataFileManager.import_file(
+			"res://data/Maps/" + map_name + "/" + map_name + "RegionGraph.txt")
+			
+			for region in region_graph_dict:
+				var node = turn_index_to_map_name(region[0], index_dict)
+				var adjacent_nodes = turn_index_to_map_name(region[1], index_dict)
+				areas[node] = adjacent_nodes
+				#forest.add_area(node, adjacent_nodes)
+		return areas
+	
+	#Methode, die den index zum Mapnamen umwandelt
+func turn_index_to_map_name(index, index_dict: Dictionary):
+		if typeof(index) == TYPE_INT:
+			return index_dict[index]
+			
+		elif typeof(index) == TYPE_ARRAY:
+			var new_array = Array()
+			for number in index:
+				new_array.append(index_dict[number])
+			return new_array
+	
+	
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
 
 func get_region_node(region_name : String):
 	pass
