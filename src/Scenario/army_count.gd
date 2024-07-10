9f5d02c6acd5d25a865eb0c9dddef252e663c424
gravitybeat <pouyan.w@gmail.com>
2024-07-10 17:51:27 +0200

    army count implemented zum teil
    army count imlemented teilweise

---
+++ src/Scenario/army_count.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+func find_army_count_label_by_name(region_name: String) -> ArmyCountLabel:
+	for child in get_children():
+		if child.region_name == region_name:
+			return child
+	return null
--- src/Scenario/scenario.gd
+++ src/Scenario/scenario.gd
@@ -2,14 +2,65 @@ extends Node2D
 
 @onready var player_timer : Timer = $"../CanvasLayer/ScenarioOverlay/PanelContainerInfo/PlayerTimer"
 @onready var buildings : Node = $Buildings
-@onready var players : Node = $Players
+@onready var army_count : Node = $ArmyCount
 
 func _ready():
 	SignalBus.call_deferred("connect", "create_building", on_create_building)
 	SignalBus.call_deferred("connect", "draw_capitals_fortress", on_draw_capitals_fortress)
 	SignalBus.call_deferred("connect", "set_political_view", on_set_political_view)
 	SignalBus.call_deferred("connect", "set_physical_view", on_set_physical_view)
-	
+	SignalBus.call_deferred("connect", "show_army_counter_labels", on_show_army_counter_labels)
+	SignalBus.call_deferred("connect", "hide_army_counter_labels", on_hide_army_counter_labels)
+	SignalBus.call_deferred("connect", "update_army_counter_labels", on_update_army_counter_labels)
+	SignalBus.call_deferred("connect", "create_army_counter_labels", on_create_army_counter_labels)
+
+func on_show_army_counter_labels():
+	for region in ScenarioDataManager.scenario_region_graph.region_array:
+		var region_army_count : ArmyCountLabel = army_count.find_army_count_label_by_name(region.region_name)
+		if region.count_army() > 0:
+			region_army_count.visible = true
+		else:
+			region_army_count.visible = false
+		
+
+
+func on_hide_army_counter_labels():
+	for army_count_label in army_count.get_children():
+		army_count_label.visible = false
+
+
+func on_update_army_counter_labels():
+		for region in ScenarioDataManager.scenario_region_graph.region_array: 
+			var region_army_count : ArmyCountLabel = army_count.find_army_count_label_by_name(region.region_name)
+			var army_count : int = region.count_army()
+			region_army_count.text = str(army_count)
+			if army_count > 0:
+				region_army_count.visible = true
+			else:
+				region_army_count.visible = false
+			
+			
+			
+			
+func on_create_army_counter_labels():
+	for region in ScenarioDataManager.scenario_region_graph.region_array:
+			var army_count_label : ArmyCountLabel = ArmyCountLabel.new()
+			var position_x = ScenarioDataManager.building_coordinates[region.region_name][0]
+			var position_y = ScenarioDataManager.building_coordinates[region.region_name][1]
+			army_count_label.region_name = region.region_name
+			army_count_label.position.x = position_x
+			army_count_label.position.y = position_y
+			
+			army_count_label.text = str(region.count_army())
+			
+			if region.count_army() == 0:
+				army_count_label.visible = false
+			
+			
+			
+			army_count.add_child(army_count_label)
+			
+			
 func on_set_physical_view():
 	buildings.visible = true
 
